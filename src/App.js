2e68221ef174cefee11796cd485262cc9c5df935
REDVINA <meruguajay.babu@atcs.com>
2021-02-11 13:14:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+// var FylFdrVap = require('fs')
+// var FylTxtVar = FylFdrVap.readFile("data.json").toJson();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//     console.log(FylTxtVar)
+import data from './data.json';
+
+class App extends Component {
+  render() {
+    console.log(data);
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Offer price</th>
+              <th>Start At</th>
+              <th>End At</th>
+              <th>Create At</th>
+              <th>Update At</th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {data.employees.map((val, i) => <tr key={i}>
+               <td>{val.product_name}</td>
+               <td>{val.product_description}</td>
+                 <td>{val.price}</td>
+               <td>{val.offer_price}</td>
+               <td>{val.offer_start_at}</td>
+               <td>{val.offer_end_at}</td>
+               <td>{val.created_at}</td>
+               <td>{val.updated_at}</td>
+              </tr>
+  )}
+          </tbody>
+        </table>
+          )
+      </div>
+    );
+  }
 }
+
 
 export default App;
